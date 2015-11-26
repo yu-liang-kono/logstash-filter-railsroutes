d68d90ddc1e136be13f6850f1da7c5e047c335b9
Yu Liang <yu.liang@thekono.com>
2015-11-26 19:38:26 +0800

    Write routes spec

--- spec/filters/railsroutes_spec.rb
+++ spec/filters/railsroutes_spec.rb
@@ -1,21 +1,60 @@
 # encoding: utf-8
 require 'spec_helper'
-require "logstash/filters/example"
+require "logstash/filters/railsroutes"
 
-describe LogStash::Filters::Example do
-  describe "Set to Hello World" do
-    let(:config) do <<-CONFIG
-      filter {
-        example {
-          message => "Hello World"
-        }
-      }
-    CONFIG
+describe LogStash::Filters::RailsRoutes do
+  subject { LogStash::Filters::RailsRoutes.new(config) }
+
+  let(:attrs) { Hash[] }
+  let(:event) { LogStash::Event.new(attrs) }
+  let(:config) do
+    {
+      'verb_source' => 'verb',
+      'uri_source' => 'uri',
+      'routes_spec' => @route_spec_filename
+    }
+  end
+
+  before(:each) do
+    file = Tempfile.new('railsroutes')
+    file.write(routes_spec_content)
+    file.close
+    @route_spec_filename = file.path
+  end
+
+  after(:each) do
+    File.exists?(@route_spec_filename) && File.unlink(@route_spec_filename)
+  end
+
+  before(:each) do
+    subject.register()
+  end
+
+  describe 'routes spec content' do
+    context 'when prefix exists in routes spec' do
+      let(:attrs) { Hash['verb', 'GET', 'uri', '/resources/1'] }
+      let(:routes_spec_content) do <<-SPEC
+        resources GET /resources/:id(.:format) resources#show
+      SPEC
+      end
+
+      it 'can match' do
+        subject.filter(event)
+        expect(event['controller#action']).to eq 'resources#show'
+      end
     end
 
-    sample("message" => "some text") do
-      expect(subject).to include("message")
-      expect(subject['message']).to eq('Hello World')
+    context 'when prefix does not exist in routes spec' do
+      let(:attrs) { Hash['verb', 'GET', 'uri', '/resources/1'] }
+      let(:routes_spec_content) do <<-SPEC
+        GET /resources/:id(.:format) resources#show
+      SPEC
+      end
+
+      it 'can match' do
+        subject.filter(event)
+        expect(event['controller#action']).to eq 'resources#show'
+      end
     end
   end
 end
